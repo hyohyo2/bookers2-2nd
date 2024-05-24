6e00fc848f96f0007d833b432f24e41e1146def1
hyohyo2 <hono.handball.0910@gmail.com>
2024-05-24 09:17:40 +0000

    検索機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
-
+  get 'search' => 'searches#search'
   resources :users, only: [:index, :show, :edit, :update] do
-    resources :relationships, only: [:create, :destroy]
+    resource :relationships, only: [:create, :destroy]
       get "followings" => "relationships#followings", as: "followings"
       get "followers" => "relationships#followers", as: "followers"
   end
